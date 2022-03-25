31e277a98c5790e421326ce49cc268c80c18a480
josemg-23 <jose.gomezt19@gmail.com>
2022-03-25 15:55:14 +0000

    four commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,17 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let i;
+  let j;
+  let k;
+  for (i = 0; i < pronoun.length; i++) {
+    for (j = 0; j < adj.length; j++) {
+      for (k = 0; k < noun.length; k++) {
+        console.log(`${pronoun[i]}` + `${adj[j]}` + `${noun[k]}`);
+      }
+    }
+  }
 };
