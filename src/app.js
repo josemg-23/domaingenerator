acdb89cf92eff5b3b686c34c2c259e15523ada28
josemg-23 <jose.gomezt19@gmail.com>
2022-03-25 16:02:48 +0000

    commit five

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = function generator() {
   //write your code here
   let pronoun = ["the", "our"];
   let adj = ["great", "big"];
@@ -16,8 +16,10 @@ window.onload = function() {
   for (i = 0; i < pronoun.length; i++) {
     for (j = 0; j < adj.length; j++) {
       for (k = 0; k < noun.length; k++) {
-        console.log(`${pronoun[i]}` + `${adj[j]}` + `${noun[k]}`);
+        let domain = `${pronoun[i]}` + `${adj[j]}` + `${noun[k]}`;
+        return console.log(domain);
       }
     }
+    generator();
   }
 };
