1012882b33e317c0ea29413ca2d94705b23eb464
josemg-23 <jose.gomezt19@gmail.com>
2022-03-26 00:37:44 +0000

    done

--- src/app.js
+++ src/app.js
@@ -5,21 +5,19 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function generator() {
-  //write your code here
-  let pronoun = ["the", "our"];
-  let adj = ["great", "big"];
-  let noun = ["jogger", "racoon"];
-  let i;
-  let j;
-  let k;
-  for (i = 0; i < pronoun.length; i++) {
-    for (j = 0; j < adj.length; j++) {
-      for (k = 0; k < noun.length; k++) {
-        let domain = `${pronoun[i]}` + `${adj[j]}` + `${noun[k]}`;
-        return console.log(domain);
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
+var domain = [];
+function Generarcomb() {
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        domain.push(pronoun[i] + adj[j] + noun[k] + ".com");
       }
     }
-    generator();
   }
-};
+}
+
+Generarcomb();
+console.log(domain);
